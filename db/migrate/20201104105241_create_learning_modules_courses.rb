b4a6e284fbc1c91211e49a89262fe65943fe3151
Robin Ginter <robinginter@gmail.com>
2020-11-05 16:26:04 +0200

    Fixes typo

--- db/migrate/20201104105241_create_learning_modules_courses.rb
+++ db/migrate/20201104105241_create_learning_modules_courses.rb
@@ -1,4 +1,4 @@
-class CreateModulesCourses < ActiveRecord::Migration[6.0]
+class CreateLearningModulesCourses < ActiveRecord::Migration[6.0]
   def change
     create_join_table :learning_modules, :courses do |t|
       t.index :learning_module_id
